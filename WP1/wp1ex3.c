345357ddf5d5fabba69b639b45ae344d7b7e8eb0
Marble879 <younisakel879@gmail.com>
2022-01-25 14:04:20 +0100

    Add TA feedback
    Co-authored-by: puerto-hub <guspuema@student.gu.se>
    Co-authored-by: MarkusJ299 <gusjarmap@student.gu.se>

--- WP1/wp1ex3.c
+++ WP1/wp1ex3.c
@@ -9,12 +9,20 @@
 #include <time.h>
 
 // Define Section
-#define MAX_NUMBER 10 // Defines the max number of guesses allowed
-#define RANGE_MAX 100 // Defines the upper bound of the range for a random number
-#define RANGE_MIN 1   // Defines the lower bound of the range for a random number
+#define MAX_NUMBER 10                                                                                   // Defines the max number of guesses allowed
+#define RANGE_MAX 100                                                                                   // Defines the upper bound of the range for a random number
+#define RANGE_MIN 1                                                                                     // Defines the lower bound of the range for a random number
+#define INITIAL_MSG "Guess the random number between 1-100:\n"                                          // Defines a message that is shown when the game starts
+#define TOO_HIGH "Your guess is too high. Try again:\n"                                                 // Defines a message when the guess is too high
+#define TOO_LOW "Your guess is too low. Try again:\n"                                                   // Defines a message when the guess is too low
+#define INCORRECT_CHOICE "Choice not registered! Please input a value between 1 and 100 (inclusive)!\n" // Defines a message when the guess entered is not valid
+#define CORRECT_GUESS_MSG "You have guessed %d times and your guess is correct\n\n"                     // Defines a message for when the user makes a correct guess //TODO: test this
+#define EXCEED_GUESSES_MSG "You have exceeded %d guesses!\n"                                            // Defines a message when the amount of guesses reaches the limit
+#define MENU_MSG "Select an option:\n1. Play again\n2. Exit\n"                                          // Defines a message where the user is presented with a menu to play the game again or exit
+#define EXITING_MSG "Exiting..\n"                                                                       // Defines a message when ending/exiting the program
 
 // Random number generator adapted from this example: https://stackoverflow.com/questions/1190870/i-need-to-generate-random-numbers-in-c
-int randomValueGenerator(int upperRange, int lowerRange)
+int generateRandNumber()
 {
     int randomNumber; // Holds the random number generated
 
@@ -23,13 +31,13 @@ int randomValueGenerator(int upperRange, int lowerRange)
 
     // Calculates a random number in the range of 1 - 100. In our case, the part in the parenthesis would evaluate to a randomNumber between 0 and 99. Hence, we add the lower bound to ensure that the number is between 1 and 100.
     // Reference: https://www.includehelp.com/c-programs/generate-random-numbers-within-a-range.aspx
-    randomNumber = (rand() % (upperRange - lowerRange + 1)) + lowerRange;
+    randomNumber = (rand() % (RANGE_MAX - RANGE_MIN + 1)) + RANGE_MIN;
 
     return randomNumber; // return the generated number
 }
 
 // Contains a loop and checker on the number of times a user guessed, and also outputs messages depending on the user input.
-void userGuesser(int randomNumber)
+void userGuess(int randomNumber)
 {
     int userInputGuess; // Holds the user guess input
     int guessCount = 0; // Counts the number of guesses
@@ -38,7 +46,7 @@ void userGuesser(int randomNumber)
     do
     {
         // Output message to console
-        printf("Guess the random number between 1-100:\n");
+        printf(INITIAL_MSG);
         // Take an input from the user
         scanf("%d", &userInputGuess);
 
@@ -51,19 +59,19 @@ void userGuesser(int randomNumber)
             if (userInputGuess > randomNumber)
             {
                 // prints to console
-                printf("Your guess is too high. Try again:\n");
+                printf(TOO_HIGH);
             }
             // Condition where the guess is too low, increases guess counter and asks for another guess
             else if (userInputGuess < randomNumber)
             {
                 // prints to console
-                printf("Your guess is too low. Try again:\n");
+                printf(TOO_LOW);
             }
             // Condition where the guess is correct, prints the guess count
             else if (userInputGuess == randomNumber)
             {
                 // prints to console and exits method.
-                printf("You have guessed %d times and your guess is correct\n\n", guessCount);
+                printf(CORRECT_GUESS_MSG, guessCount);
                 return;
             }
         }
@@ -71,41 +79,41 @@ void userGuesser(int randomNumber)
         else
         {
             //output message.
-            printf("Choice not registered! Please input a value between 1 and 100 (inclusive)!\n");
+            printf(INCORRECT_CHOICE);
         }
         // Keep looping while the user has not guessed the random number, and the guess count is below 10.
     } while (userInputGuess != randomNumber && guessCount < MAX_NUMBER);
 
     // Output message to console.
-    printf("You have exceeded %d guesses!\n", MAX_NUMBER);
+    printf(EXCEED_GUESSES_MSG, MAX_NUMBER);
 }
 
 // Sets up the game session, calls the guessing game session and calls a random value generator.
-void gameSession()
+void startGame()
 {
     int userInputNewRound; // Declares the "start new round" value
     // Loops the gamesession at least once, and for as long as the user selects to play again
     do
     {
-        int randomNumber = randomValueGenerator(RANGE_MAX, RANGE_MIN); // Calls the randomValuleGenerator() and stores the returned value to an int variable
+        int randomNumber = generateRandNumber(); // Calls the randomValuleGenerator() and stores the returned value to an int variable
         // Method that starts one game session
-        userGuesser(randomNumber);
+        userGuess(randomNumber);
         // Prints the options to continue the game or exit
-        printf("Select an option:\n1. Play again\n2. Exit\n");
+        printf(MENU_MSG);
         // Takes the answer input from the user
         scanf("%d", &userInputNewRound);
 
         // Continues to loop unless the user inputs 2
     } while (userInputNewRound != 2);
     // Prints message when exiting the game session
-    printf("Exiting..\n");
+    printf(EXITING_MSG);
 }
 
 // Main function in the program, no program arguments supported
 int main()
 {
     // Method that begins the guessing game session
-    gameSession();
+    startGame();
     // Exits program
     return 0;
 }
