e32d178472198845670695b783795efb0ec958f0
Marble879 <younisakel879@gmail.com>
2022-01-25 14:16:39 +0100

    Add TA feedback to exercise 2 wp1
    Co-authored-by: puerto-hub <guspuema@student.gu.se>
    Co-authored-by: MarkusJ299 <gusjarmap@student.gu.se>

--- WP1/wp1ex2.c
+++ WP1/wp1ex2.c
@@ -8,7 +8,15 @@
 #include <stdlib.h>
 
 // Defines Section
-#define MAX 50 // defines max array size.
+#define MAX 50                                                           // defines max array size.
+#define INPUT_REQUEST_MSG "Please input a string you want to encrypt:\n" // defines the input request message
+#define CONTINUE "Type another text or use OS EOF:\n"                    // defines message whentext has been encrypted
+#define NEW_WORD "New word: %s"                                          // defines a message for printing the new word
+#define EOL '\0'                                                         // defines EOL character
+#define NEW_LINE '\n'                                                    // defines the new line character
+#define SPACE_WHITESPACE ' '                                             // defines a space
+#define END_CHAR_LOWER 'z'                                               // defines the final letter in the alphabet (in lower case)
+#define END_CHAR_UPPER 'Z'                                               // defines the final letter in alphabet (in upper case)
 
 // Method to shift the character given in the parameter
 char characterShifter(char cToShift, int numShifts)
@@ -17,7 +25,7 @@ char characterShifter(char cToShift, int numShifts)
     for (int i = 0; i < numShifts; i++)
     {
         // Checks whether the end of the alphabet is reached and handles it accordingly.
-        if ((cToShift == 'z' || cToShift == 'Z'))
+        if ((cToShift == END_CHAR_LOWER || cToShift == END_CHAR_UPPER))
         {
             // Character loops back and becomes a or A.
             cToShift = cToShift - 25; // counts as one move forward.
@@ -41,7 +49,7 @@ int main(int argc, char *argv[])
     char stringToShift[MAX];         // Declare array that is of size MAX (50)
 
     // Output a message requesting a user input
-    printf("Please input a string you want to encrypt:\n");
+    printf(INPUT_REQUEST_MSG);
 
     // While EOF has not been inputted, keep looping and retreiving user input.
     while (fgets(stringToShift, MAX, stdin) != NULL)
@@ -51,11 +59,11 @@ int main(int argc, char *argv[])
         {
             charToShift = stringToShift[i]; // Assign the character that may be shifted from the input string.
             // Check if we have reached the new line or end of string
-            if (charToShift == '\0' || charToShift == '\n')
+            if (charToShift == EOL || charToShift == NEW_LINE)
                 // Break out of loop once \0 or \n occurs.
                 break;
             // If there is no space, then shift the character, else leave the space as is.
-            if (charToShift != ' ')
+            if (charToShift != SPACE_WHITESPACE)
             {
                 //Shift characters by the number of shifts
                 shifted = characterShifter(charToShift, numOfShifts);
@@ -64,10 +72,10 @@ int main(int argc, char *argv[])
             }
         }
         //Output shifted string
-        printf("New word: %s", stringToShift);
+        printf(NEW_WORD, stringToShift);
 
         //Output input prompt
-        printf("Type another text or use OS EOF:\n");
+        printf(CONTINUE);
     }
 
     // Exits program
