991ea558ed2beb7c10af0f214caf1af9b9433fc2
Marble879 <younisakel879@gmail.com>
2022-03-02 14:47:24 +0100

    Add solution to wp6 ex1 task 2
    Co-authored-by: puerto-hub <guspuema@student.gu.se>
    Co-authored-by: MarkusJ299 <gusjarmap@student.gu.se>

--- WP6/EX1/WP6_Ex1_task2.ino
+++ WP6/EX1/WP6_Ex1_task2.ino
@@ -1,9 +1,5 @@
-// (C) Markus Emilio Puerto Gutiérrez, Markus Järveläinen, Younis Akel, group: 15 (2022)
-// Work package 6
-// Exercise 1 Task 2
-// Submission code:  (provided by your TA-s)
+// WP 6 Exercise 1 Template DIT 632
 
-// Defines section
 #define ENCA 2
 #define ENCB 3
 #define PWM1 5
@@ -16,13 +12,9 @@ int degtarget = 0; // Target position in degrees
 
 int speed = 0; // Desired motor speed
 
-int kp = 5;    // Proportional constant for controller (tuning parameter)
-int u_out = 0; // output of controller
-
 int e = 0; // error
 int a = 0; // a-encoder signal
 int b = 0; // b-encoder signal
-int go = 0;
 
 void setup()
 {
@@ -34,13 +26,11 @@ void setup()
     pinMode(PWM2, OUTPUT);
 
     /*
-    //TODO: TASK 2
     ==> TO DO TO DO TO DO
     ATTACH INTERRUPT HERE.
     */
-
-    // Reference: https://create.arduino.cc/projecthub/curiores/how-to-control-a-dc-motor-with-an-encoder-d1734c
     attachInterrupt(digitalPinToInterrupt(ENCA), ISR_readEncoder, RISING);
+    attachInterrupt(digitalPinToInterrupt(ENCB), ISR_readEncoder, RISING);
 
     // Start the motor, just a tiny little bit because otherwise TinkerCad dies....
     analogWrite(PWM2, 10);
@@ -68,7 +58,7 @@ void loop()
     }
 
     // Print current position
-    Serial.print("The current position is: ");
+    Serial.print("The current degree is: ");
     Serial.print(deg);
     Serial.print("\n");
 
@@ -78,20 +68,17 @@ void loop()
     // Calculate initial error
     e = degtarget - deg;
 
-    go = 1;
-
     // Loop until error is zero
     while (e)
     {
-
         // Map current position into degrees
         deg = map(pos, 0, 2299, 0, 359);
-        Serial.println("in loop");
 
         // Get necessary speed signal
-        // speed = getAction(e);
-
-        speed = 100;
+        if (degtarget >= deg)
+            speed = 100;
+        else
+            speed = -100;
 
         // Send speed signal to motor
         // Rotating clockwise
@@ -100,12 +87,8 @@ void loop()
             if (speed < 100) // motor does not react with too low inputs
                 speed = 100;
             analogWrite(PWM2, 0);
-
-            analogWrite(PWM1, speed); // TODO: NOTE: if comment this out, while loop goes as normal.
-                                      //  Is interrupt spammed? But is interupt not called once,
-                                      //  then we move on with loop?
+            analogWrite(PWM1, speed);
         }
-
         // Rotating counter-clockwise
         else
         {
@@ -118,8 +101,6 @@ void loop()
         // Calculate new error
         e = degtarget - deg;
     }
-    Serial.println("OUTSIDE LOOP");
-    go = 0;
 }
 
 int getInput()
@@ -129,7 +110,7 @@ int getInput()
     char buf[3];
     int input = -1;
 
-    Serial.print("Please enter the desired position: \n");
+    Serial.print("Please enter the desired degree: \n");
 
     while (!ready)
     {
@@ -140,61 +121,16 @@ int getInput()
     return input;
 }
 
-int getAction(int error)
-{
-    /*
-    //TODO: TASK 3
-    ==> TO DO TO DO TO DO
-    Calculate u_out as function of the error and the kp (tuning parameter).
-    */
-
-    // u_out = kp*e;
-
-    if (u_out > 254)
-    { // u_out cannot be more than 255...
-        return 255;
-    }
-    else if (u_out < -254)
-    { //...or less than -254
-        return -255;
-    }
-    else
-        return u_out;
-}
-
 void ISR_readEncoder()
 {
-    // if (go == 1)
-    //{
-    /*
-//TODO: task 2
-==> TO DO TO DO TO DO
-READ THE ENCODER SIGNAL HERE.
-Read the encoder signals and increase or decrease pos accordingly.
-*/
-    Serial.print("ERROR: ");
-    Serial.println(e);
-    // Reference: https://create.arduino.cc/projecthub/curiores/how-to-control-a-dc-motor-with-an-encoder-d1734c
     b = digitalRead(ENCB);
-    // a = digitalRead(ENCA);
-    //  If b is greater than 0, we know b must be ahead of a,
-    //  hence signal A precedes signal B and the motor is moving
-    //  clockwise
-    if (b == 0)
+    a = digitalRead(ENCA);
+    if (a > 0 && b == 0)
     {
-        // increment the position
         pos++;
     }
-    // Else If b is less than or equal to 0, we know b must be before a,
-    // hence signal B precedes signal A and the motor is moving
-    // anticlockwise
-    else
+    else if (b > 0 && a == 0)
     {
-        // decrement the position
         pos--;
     }
-    Serial.print("Pos is: ");
-    Serial.println(pos);
-    //}
 }
-// TODO: requirem,ent to read both A and B
